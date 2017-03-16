9116aa92e39fd4a5cc188a13d1fe0708abd7d992
qw789 <791398818@qq.com>
2017-03-16 15:27:18 +0800

    child-parent

--- src/pages/test/test1.vue
+++ src/pages/test/test1.vue
@@ -4,26 +4,56 @@
     <router-link to="/test/test2">前往test2页面</router-link>
     <h2>this is  a todolist</h2>
     <ul>
-      <li v-for="item in items" :class='{decoration:item.isFinished}'>
+      <li v-for="item in items" :class='{decoration:item.isFinished}' @click="toggleFinish(item)">
          {{item.lable}}
       </li>
     </ul>
+    <h1>儿子告诉我{{childWord}}</h1>
+    <input type="" name="" v-model="newItem" v-on:keyup.enter="addNew()">
+    <component-a msgFromFather="you die" v-on:childWay="sonSay"></component-a>
   </div>
 </template>
 
 <script>
+import Storage from "../../storage.js"
+import componentA from './test3.vue'
 export default {
   name: 'test1',
   data () {
     return {
       msg: 'Welcome to Your test1 page',
-      items:[
-      {lable:"eat",isFinished:true},
-      {lable:'working',isFinished:true}
-      ]
+      items:Storage.fetch(),
+      newItem:"",
+      childWord:"11"
     }
   },
   methods:{
+    toggleFinish(aa){
+      return aa.isFinished=!aa.isFinished
+    },
+    addNew(){
+      console.log(this.newItem)
+      this.items.push({
+         lable:this.newItem,
+         isFinished:true
+      })
+      this.newItem="";
+    },
+    sonSay(msg){
+      console.log(msg)
+      this.childWord=msg;
+    }
+  },
+   watch:{
+    items:{
+     handler(items) { 
+      Storage.save(items)
+     },
+      deep: true
+    }
+  },
+  components:{
+    componentA
   }
 }
 </script>
@@ -31,5 +61,8 @@ export default {
 <style scoped>
 a {
   color: #42b983;
+}
+.decoration{
+  text-decoration: underline;
 }
 </style>
---
+++ src/pages/test/test3.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="hello">
+		<h1>{{msg}}{{msgFromFather}}</h1>
+		<button @click="onclickMe">fuck!</button>
+	</div>
+</template>
+<script type="text/javascript">
+	export default{
+		data(){
+			return{
+				msg:"hello from componenta"
+			}
+		},
+		props:['msgFromFather'],
+		methods:{
+			onclickMe(){
+				console.log(this.msg)
+				this.$emit("childWay",this.msg)
+			}
+		}
+	}
+</script>
